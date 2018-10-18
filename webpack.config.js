e4a5123ecd5dbd5c3c426f4e208c9f3a1b3f22e9
Tzechiu Lei <tclei2009@gmail.com>
2018-10-18 16:55:27 -0700

    Set up webpack prod build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,56 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: [
-    './src/app.js',
-    './src/scss/app.scss'
-  ],
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
-        test: /\.s?css$/,
-      }
+module.exports = (env) => {
+  console.log('env:', env);
+  const isProd = env === 'production';  // set up in package.json.
+  const CSSExtract = new ExtractTextPlugin('bundle.css');
+
+  return {
+    entry: [
+      './src/app.js',
+      './src/scss/app.scss'
     ],
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-    port: 8888,
-  },
-};
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          }),
+          test: /\.s?css$/,
+        }
+      ],
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      port: 8888,
+    },
+  };
+}
