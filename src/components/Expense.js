67ebb1b1d7c7a9cc3b99fa38e79d6814a1bb05ef
Tzechiu Lei <tclei2009@gmail.com>
2018-10-18 18:58:10 -0700

    Format expense-item display.

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const Expense = (props) => (
   <div id={`expense-${props.id}`} className="expense component">
-    <span>{props.createdAt}:</span>&nbsp;
+    <span>{moment(props.createdAt).format('MM/DD/YYYY')}:</span>&nbsp;
     <span>{props.description}</span>&nbsp;
-    <span>{props.amount}</span>&nbsp;
+    <span>{numeral(props.amount / 100).format('$0,0.00')}</span>&nbsp;
     <Link to={`/edit/${props.id}`}>Edit</Link>
   </div>
 );
