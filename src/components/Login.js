2e5049646120d0f5c70fa5cf1eba01fb04f8ff2b
Tzechiu Lei <tclei2009@gmail.com>
2018-10-30 22:57:57 -0700

    Refine content/styling further.

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,8 +5,10 @@ import { startLogin } from '../actions/authActions';
 export const Login = (props) => (
   <div className="login component">
     <h1 className="login-title">React-Expensify</h1>
+    <p className="login-preamble"><span className="highlight">DEMO for TESTING ONLY</span>: INPUT ONLY IMAGINARY EXPENSES!  Do NOT use this as your real expense-tracker.</p>
     <p className="login-instructions">Please log in to your expenses:</p>
-    <button className="login-btn" onClick={props.startLogin}>Log in with Google</button>
+    <button className="login-btn third-party google" onClick={props.startLogin} title="Log in with Google"></button>
+    <p className="login-credits">This DEMO app uses <a href="https://firebase.google.com/" target="_blank">Google Firebase</a> Authentication & Databases.</p>
   </div>
 );
 
