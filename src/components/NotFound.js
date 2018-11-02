84ced351ad1283b847fc80db3def1948df6c1aa6
Tzechiu Lei <tclei2009@gmail.com>
2018-11-02 01:02:36 -0700

    Secure dashboard link display in NotFound error-view.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => (
+const NotFound = (props) => (
   <div>
     <h2>404 -- Not Found</h2>
     <p>Sorry!  The resource you requested was not found.</p>
-    <p>You can go to <Link to="/">Expense Dashboard</Link>.</p>
+    {props.isAuthenticated ?
+      <p>You can go to <Link to="/dashboard">Expense Dashboard</Link>.</p> :
+      <p>You can go to <Link to="/">Login</Link>.</p>
+    }
   </div>
 );
 
-export default NotFound;
+const mapStateToProps = (state) => ({
+  isAuthenticated: typeof state.auth.user !== undefined
+});
+
+export default connect(mapStateToProps, undefined)(NotFound);
