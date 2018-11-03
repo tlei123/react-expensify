7fac1f79af46227df8a6ac620d62563da4f62380
Tzechiu Lei <tclei2009@gmail.com>
2018-11-02 19:18:30 -0700

    Refactor Login buttons responsive-styling.

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,10 +8,12 @@ export const Login = (props) => (
     <p className="login-byline">by <a href="http://tze1.com/" title="Tze-chiu Lei's portfolio site" target="_blank">Tze-chiu Lei</a></p>
     <p className="login-preamble"><span className="highlight">DEMO for TESTING ONLY</span>: INPUT ONLY IMAGINARY EXPENSES!  Do NOT use this as your real expense-tracker.</p>
     <p className="login-instructions">Please log in to your expenses:</p>
-    <button className="login-btn btn third-party google" onClick={props.startLoginGoogle} title="Log in with Google">Login with Google</button>
-    <button className="login-btn btn third-party facebook" onClick={props.startLoginFacebook} title="Log in with Facebook">Continue with Facebook</button>
-    <button className="login-btn btn third-party twitter" onClick={props.startLoginTwitter} title="Log in with Twitter">Login with Twitter</button>
-    <button className="login-btn btn third-party github" onClick={props.startLoginGithub} title="Log in with GitHub">Login with GitHub</button>
+    <div className="login-btns">
+      <button className="login-btn btn third-party google" onClick={props.startLoginGoogle} title="Log in with Google">Login with Google</button>
+          <button className="login-btn btn third-party facebook" onClick={props.startLoginFacebook} title="Log in with Facebook">Continue with Facebook</button>
+      <button className="login-btn btn third-party twitter" onClick={props.startLoginTwitter} title="Log in with Twitter">Login with Twitter</button>
+      <button className="login-btn btn third-party github" onClick={props.startLoginGithub} title="Log in with GitHub">Login with GitHub</button>
+  </div>
     <p className="login-credits">This DEMO app uses <a href="https://firebase.google.com/" target="_blank">Google Firebase</a> Authentication & Databases.<br />NOTE: To test multiple sign-in methods, first ensure that each method is associated with a different email address.</p>
     <p className="login-appinfo">See source-code and unit-tests in the <a href="https://github.com/tlei123/react-expensify" target="_blank">GitHub project</a>.</p>
   </div>
