816f8f11d1cc253d39562809e448be309de53118
Tzechiu Lei <tclei2009@gmail.com>
2018-11-17 22:24:09 -0700

    Add ConfirmModal component for removing expenses.

---
+++ src/components/ConfirmModal.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+/* Usage:
+* import ConfirmModel from '<path-from-importer-to-ConfirmModal>';
+* <Modal
+*   show={this.state.showConfirmModal}
+*   title='<your-optional-title>'
+*   content='<your-required-content>'
+*   btnLabelCancel='<your-optional-cancel-btn-label>'
+*   btnLabelOk='<your-optional-ok-btn-label>'
+*   handleCancel={this.handleCancel}
+*   handleOk={this.handleOk}
+* />
+*/
+
+const ConfirmModal = (props) => (
+  <Modal className="confirmmodal component modal-content"
+    overlayClassName="modal-overlay"
+    isOpen={ props.show }
+    role='dialog'
+    shouldCloseOnOverlayClick={false}
+    contentLabel={ props.title ? props.title : 'Confirm' }
+  >
+    <h3 className="modal-title">{ props.title ? props.title : 'Confirm' }</h3>
+    <p className="modal-message">{ props.content }</p>
+    <div className="modal-actions">
+      <button onClick={ props.handleCancel } className="modal-cancel btn exit">
+        { props.btnLabelCancel ? props.btnLabelCancel : 'Cancel' }
+      </button>
+      <button onClick={ props.handleOk } className="modal-ok btn danger">
+        { props.btnLabelOk ? props.btnLabelOk : 'OK' }
+      </button>
+    </div>
+  </Modal>
+);
+
+export default ConfirmModal;
