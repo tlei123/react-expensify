55b457b425261b3742ce72a5197270b2a94c1418
Tze Lei <admin@tze1.com>
2019-07-27 22:47:50 -0700

    #3 - Build-up e2e test spec.

--- src/tests/e2e/react-expensify.e2e.spec.js
+++ src/tests/e2e/react-expensify.e2e.spec.js
@@ -1,10 +1,14 @@
-// TODO: Populate this suite w/ real tests.
+// TODO: Implement app start-up and login commans if possible.
 module.exports = {
-  'React-Expensify e2e test': function (browser) {
+  'React-Expensify e2e test: dashboard': function (browser) {
     browser
       .url('localhost:8888')
       .waitForElementVisible('body')
       .expect.element('h1.login-title').text.to.contain('React-Expensify');
+
+    browser
+    	.waitForElementVisible('.login-btn.facebook')
+      .expect.element('.login-btn.facebook').text.to.contain('Facebook');
 
     browser.end();
   }
