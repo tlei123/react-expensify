6b80271490e13dcb19c5801844d03aacac465cc3
Tze Lei <admin@tze1.com>
2019-08-14 22:21:42 -0700

    #3 - Change login helper to async; Start Add-expense test-case.

--- src/tests/e2e/helpers.js
+++ src/tests/e2e/helpers.js
@@ -5,29 +5,33 @@ function switchTestWindow (client, index) {
 }
 
 function login (client) {
-  // Homepage (Login) element selectors.
-  const loginBtnSelector = '.login-btn.google';
-  const usernameInputSelector = 'input[type=email]';
-  const passwordInputSelector = 'input[type=password]';
-  const googleLoginBtnSelector = 'button[type=submit]';
-  // App pages element selectors.
-  const topNavSelector = 'header .nav';
-  const pageTitleSelector = 'h2.page-title';
+  return new Promise(function (resolve, reject) {
+    // Homepage (Login) element selectors.
+    const loginBtnSelector = '.login-btn.google';
+    const usernameInputSelector = 'input[type=email]';
+    const passwordInputSelector = 'input[type=password]';
+    const googleLoginBtnSelector = 'button[type=submit]';
+    // App pages element selectors.
+    const topNavSelector = 'header .nav';
+    const pageTitleSelector = 'h2.page-title';
 
-  client
-    .waitForElementVisible(loginBtnSelector, 500)
-    .click(loginBtnSelector);
-  switchTestWindow(client, 1);
-  client.waitForElementVisible(usernameInputSelector, 10000, function () {
-    client.setValue(usernameInputSelector, 'tze1testuser1@gmail.com', function () {
-      client.keys(client.Keys.ENTER);
+    client
+      .waitForElementVisible(loginBtnSelector, 500)
+      .click(loginBtnSelector);
+
+    switchTestWindow(client, 1);
+    client.waitForElementVisible(usernameInputSelector, 10000, function () {
+      client.setValue(usernameInputSelector, 'tze1testuser1@gmail.com', function () {
+        client.keys(client.Keys.ENTER);
+      });
     });
-  });
-  client.pause(1000);
-  client.waitForElementVisible(passwordInputSelector, 3000, function () {
-    client.setValue(passwordInputSelector, 'k;klL*6bP7Y', function () {
-      client.keys(client.Keys.ENTER, function () {
-        switchTestWindow(client, 0);
+    client.pause(1000);
+    client.waitForElementVisible(passwordInputSelector, 5000, function () {
+      client.setValue(passwordInputSelector, 'k;klL*6bP7Y', function () {
+        client.keys(client.Keys.ENTER, function () {
+          switchTestWindow(client, 0);
+          resolve('Login finished.');
+        });
       });
     });
   });
--- src/tests/e2e/react-expensify.e2e.spec.js
+++ src/tests/e2e/react-expensify.e2e.spec.js
@@ -4,15 +4,32 @@ const Helpers = require('./helpers.js');
 var testExpenses = require('../fixtures/testExpenses');
 
 module.exports = {
-  'React-Expensify e2e test': function (client) {
+  'Start-up & Login': function (client) {
     client
       .url('localhost:8888')
       .waitForElementVisible('body')
       .expect.element('h1.login-title').text.to.contain('React-Expensify');
 
-    // Login
-    Helpers.login(client);
-    
-    client.waitForElementVisible('.expensedashboard.component', 10000);
+    Helpers.login(client)
+      .then(
+        function () {
+          client.waitForElementVisible(
+            '.expensedashboard.component',
+            10000,
+            false
+          );
+        }
+      );
+  },
+
+  'Add expense': function (client) {
+    client.waitForElementVisible('.header-nav a[href="/add"]', 500)
+      .click('.header-nav a[href="/add"]')
+      .waitForElementVisible('.expenseform.component', 1000);
+    client.expect.element('input[name=date]').to.be.visible;
+    client.expect.element('input[name=description]').to.be.visible;
+    client.expect.element('input[name=amount]').to.be.visible;
+    client.expect.element('textarea[name=note]').to.be.visible;
+    client.expect.element('button[type=submit]').to.be.visible;
   }
 };
