4ea53622f5fc2fdd1cf96832ea3f4be98dc3e991
Tze Lei <admin@tze1.com>
2019-12-02 12:05:46 -0700

    Refactor reducer unit-tests to use BDD format.

--- src/tests/reducers/filtersReducer.test.js
+++ src/tests/reducers/filtersReducer.test.js
@@ -1,52 +1,54 @@
 import moment from 'moment';
 import filtersReducer from '../../reducers/filtersReducer';
 
-test('Should set up default filter values', () => {
-  const newState = filtersReducer(undefined, { type: '@@INIT' });
+describe('filtersReducer', () => {
+  it('Should set up default filter values', () => {
+    const newState = filtersReducer(undefined, { type: '@@INIT' });
 
-  expect(newState).toEqual({
-    text: '',
-    sortBy: 'date',
-    startDate: moment().subtract(30, 'days'),
-    endDate: undefined
-  });
-});
-
-test('Should set filter-text to provided value', () => {
-  const newState = filtersReducer(undefined, { type: 'SET_FILTER_TEXT', text: 'e'});
-
-  expect(newState.text).toBe('e');
-});
-
-test('Should set filter-startDate to provided value', () => {
-  const testStartDate = moment(0),
-    newState = filtersReducer(undefined, { type: 'SET_FILTER_STARTDATE', date: testStartDate });
-
-  expect(newState.startDate).toBe(testStartDate);
-});
-
-test('Should set filter-endDate to provided value', () => {
-  const testEndDate = moment().endOf('month').add(2, 'days'),
-    newState = filtersReducer(undefined, { type: 'SET_FILTER_ENDDATE', date: testEndDate });
-
-  expect(newState.endDate).toBe(testEndDate);
-});
-
-test('Should set filter-sortBy to date', () => {
-  const currentState = {
+    expect(newState).toEqual({
       text: '',
-      sortBy: 'amount',
-      startDate: undefined,
+      sortBy: 'date',
+      startDate: moment().subtract(30, 'days'),
       endDate: undefined
-    },
-    action = { type: 'SET_FILTER_SORTBY', sortBy: 'date' },
-    newState = filtersReducer(currentState, action);
+    });
+  });
 
-  expect(newState.sortBy).toBe('date');
-});
+  it('Should set filter-text to provided value', () => {
+    const newState = filtersReducer(undefined, { type: 'SET_FILTER_TEXT', text: 'e'});
 
-test('Should set filter-sortBy to amount', () => {
-  const newState = filtersReducer(undefined, { type: 'SET_FILTER_SORTBY', sortBy: 'amount'});
+    expect(newState.text).toBe('e');
+  });
 
-  expect(newState.sortBy).toBe('amount');
+  it('Should set filter-startDate to provided value', () => {
+    const testStartDate = moment(0),
+      newState = filtersReducer(undefined, { type: 'SET_FILTER_STARTDATE', date: testStartDate });
+
+    expect(newState.startDate).toBe(testStartDate);
+  });
+
+  it('Should set filter-endDate to provided value', () => {
+    const testEndDate = moment().endOf('month').add(2, 'days'),
+      newState = filtersReducer(undefined, { type: 'SET_FILTER_ENDDATE', date: testEndDate });
+
+    expect(newState.endDate).toBe(testEndDate);
+  });
+
+  it('Should set filter-sortBy to date', () => {
+    const currentState = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+      },
+      action = { type: 'SET_FILTER_SORTBY', sortBy: 'date' },
+      newState = filtersReducer(currentState, action);
+
+    expect(newState.sortBy).toBe('date');
+  });
+
+  it('Should set filter-sortBy to amount', () => {
+    const newState = filtersReducer(undefined, { type: 'SET_FILTER_SORTBY', sortBy: 'amount'});
+
+    expect(newState.sortBy).toBe('amount');
+  });
 });
--- src/tests/reducers/messageReducer.test.js
+++ src/tests/reducers/messageReducer.test.js
@@ -1,56 +1,58 @@
 import messageReducer from '../../reducers/messageReducer';
 
-const defaultState = {
-  show: false,
-  variant: 'info',
-  content: '',
-  autoDismiss: true,
-};
-
-test('Should set up default state properly on init', () => {
-  const newState = messageReducer(undefined, { type: '@@INIT' });
-
-  expect(newState).toEqual(defaultState);
-});
-
-test('Should set state properly upon messageAddExpense action', () => {
-  const expectedState = {
-    show: true,
-    variant: 'success',
-    content: 'Expense Added',
+describe('messageReducer', () => {
+  const defaultState = {
+    show: false,
+    variant: 'info',
+    content: '',
     autoDismiss: true,
   };
-  const newState = messageReducer(undefined, { type: 'MSG_EXPENSE_ADDED' });
 
-  expect(newState).toEqual(expectedState);
-});
+  it('Should set up default state properly on init', () => {
+    const newState = messageReducer(undefined, { type: '@@INIT' });
 
-test('Should set state properly upon messageEditExpense action', () => {
-  const expectedState = {
-    show: true,
-    variant: 'success',
-    content: 'Expense Edited',
-    autoDismiss: true,
+    expect(newState).toEqual(defaultState);
+  });
+
+  it('Should set state properly upon messageAddExpense action', () => {
+    const expectedState = {
+      show: true,
+      variant: 'success',
+      content: 'Expense Added',
+      autoDismiss: true,
+    };
+    const newState = messageReducer(undefined, { type: 'MSG_EXPENSE_ADDED' });
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('Should set state properly upon messageEditExpense action', () => {
+    const expectedState = {
+      show: true,
+      variant: 'success',
+      content: 'Expense Edited',
+      autoDismiss: true,
+    };
+    const newState = messageReducer(undefined, { type: 'MSG_EXPENSE_EDITED' });
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('Should set state properly upon messageRemoveExpense action', () => {
+    const expectedState = {
+      show: true,
+      variant: 'success',
+      content: 'Expense Removed',
+      autoDismiss: true,
   };
-  const newState = messageReducer(undefined, { type: 'MSG_EXPENSE_EDITED' });
+    const newState = messageReducer(undefined, { type: 'MSG_EXPENSE_REMOVED' });
 
-  expect(newState).toEqual(expectedState);
-});
+    expect(newState).toEqual(expectedState);
+  });
 
-test('Should set state properly upon messageRemoveExpense action', () => {
-  const expectedState = {
-    show: true,
-    variant: 'success',
-    content: 'Expense Removed',
-    autoDismiss: true,
-};
-  const newState = messageReducer(undefined, { type: 'MSG_EXPENSE_REMOVED' });
+  it('Should set default state properly upon dismissMessage action', () => {
+    const newState = messageReducer(undefined, { type: 'DISMISS_MSG' });
 
-  expect(newState).toEqual(expectedState);
-});
-
-test('Should set default state properly upon dismissMessage action', () => {
-  const newState = messageReducer(undefined, { type: 'DISMISS_MSG' });
-
-  expect(newState).toEqual(defaultState);
+    expect(newState).toEqual(defaultState);
+  });
 });
