2598ad7db532b6b5b9014d5b4f2f9968fee1f307
Tze Lei <admin@tze1.com>
2019-12-02 12:07:22 -0700

    Refactor selector unit-tests to use BDD format.

--- src/tests/selectors/expensesSelectors.test.js
+++ src/tests/selectors/expensesSelectors.test.js
@@ -2,70 +2,72 @@ import moment from 'moment';
 import { filterExpenses } from '../../selectors/expensesSelectors';
 import testExpenses from '../fixtures/testExpenses.js';
 
-test('Should filter by text value', () => {
-  const testFilters = {
-      text: 'e',
+describe('expenseSelectors', () => {
+  it('Should filter by text value', () => {
+    const testFilters = {
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+      },
+      filteredExpenses = filterExpenses(testExpenses, testFilters);
+
+    expect(filteredExpenses).toEqual([ testExpenses[2], testExpenses[1] ]);
+  });
+
+  it('Should filter by start-date', () => {
+    const testFilters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined
+      },
+      filteredExpenses = filterExpenses(testExpenses, testFilters);
+
+    expect(filteredExpenses).toEqual([ testExpenses[2], testExpenses[0] ]);
+  });
+
+  it('Should filter by end-date', () => {
+    const testFilters = {
+      text: '',
+      sortBy: 'date',
+      startDate: undefined,
+      endDate: moment(0)
+    },
+    filteredExpenses = filterExpenses(testExpenses, testFilters);
+
+    expect(filteredExpenses).toEqual([ testExpenses[0], testExpenses[1] ]);
+  });
+
+  it('Should sort by date', () => {
+    const testFilters = {
+      text: '',
       sortBy: 'date',
       startDate: undefined,
       endDate: undefined
     },
     filteredExpenses = filterExpenses(testExpenses, testFilters);
 
-  expect(filteredExpenses).toEqual([ testExpenses[2], testExpenses[1] ]);
-});
+    expect(filteredExpenses).toEqual([
+        testExpenses[2],
+        testExpenses[0],
+        testExpenses[1]
+      ]);
+  });
 
-test('Should filter by start-date', () => {
-  const testFilters = {
+  it('Should sort by amount', () => {
+    const testFilters = {
       text: '',
-      sortBy: 'date',
-      startDate: moment(0),
+      sortBy: 'amount',
+      startDate: undefined,
       endDate: undefined
     },
     filteredExpenses = filterExpenses(testExpenses, testFilters);
 
-  expect(filteredExpenses).toEqual([ testExpenses[2], testExpenses[0] ]);
-});
-
-test('Should filter by end-date', () => {
-  const testFilters = {
-    text: '',
-    sortBy: 'date',
-    startDate: undefined,
-    endDate: moment(0)
-  },
-  filteredExpenses = filterExpenses(testExpenses, testFilters);
-
-  expect(filteredExpenses).toEqual([ testExpenses[0], testExpenses[1] ]);
-});
-
-test('Should sort by date', () => {
-  const testFilters = {
-    text: '',
-    sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined
-  },
-  filteredExpenses = filterExpenses(testExpenses, testFilters);
-
-  expect(filteredExpenses).toEqual([
-      testExpenses[2],
-      testExpenses[0],
-      testExpenses[1]
-    ]);
-});
-
-test('Should sort by amount', () => {
-  const testFilters = {
-    text: '',
-    sortBy: 'amount',
-    startDate: undefined,
-    endDate: undefined
-  },
-  filteredExpenses = filterExpenses(testExpenses, testFilters);
-
-  expect(filteredExpenses).toEqual([
-      testExpenses[1],
-      testExpenses[2],
-      testExpenses[0]
-    ]);
+    expect(filteredExpenses).toEqual([
+        testExpenses[1],
+        testExpenses[2],
+        testExpenses[0]
+      ]);
+  });
 });
